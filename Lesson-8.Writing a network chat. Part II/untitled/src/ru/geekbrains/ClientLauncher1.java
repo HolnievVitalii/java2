9d47c3ff5af6f9b2110f1895999d31f63277daf3
HolnievVitalii <holnievvitalii@outlook.com>
2019-11-14 14:30:14 +0300

    lesson8.Chat.Part2

---
+++ Lesson-8.Writing a network chat. Part II/untitled/src/ru/geekbrains/ClientLauncher1.java
@@ -0,0 +1,15 @@
+package ru.geekbrains;
+
+import ru.geekbrains.client.ClientWindow;
+
+/**
+ * @author ilnaz-92@yandex.ru
+ * Created on 06/09/2019
+ */
+public class ClientLauncher1
+{
+  public static void main(String[] args)
+  {
+    ClientWindow clientWindow = new ClientWindow();
+  }
+}
---
+++ Lesson-8.Writing a network chat. Part II/untitled/src/ru/geekbrains/server/ServerLauncher.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.server;
+
+/**
+ * @author ilnaz-92@yandex.ru
+ * Created on 03/09/2019
+ */
+public class ServerLauncher
+{
+
+  public static void main(String[] args)
+  {
+    Server server = new Server();
+  }
+}
